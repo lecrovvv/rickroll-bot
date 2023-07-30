4f735a1a61a1d0cdb7a945fa07f6b2d26f50a1a7
weirdoo <117682185+resweirdoo@users.noreply.github.com>
2023-07-30 20:34:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-telegram_token = "6192077542:AAFvz-DtlUym2Zd8GvLqAJ98Eq9TZvtxHsU"
+telegram_token = ""
 rickroll_link = "https://tinyurl.com/yc665n9y"
 referal_phrase = "potato"
 victims_preview = {
