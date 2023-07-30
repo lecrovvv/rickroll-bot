09237ad70043439932937018f38e7168852e6015
weirdoo <117682185+resweirdoo@users.noreply.github.com>
2023-07-30 20:34:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ telegram_token = "6192077542:AAFvz-DtlUym2Zd8GvLqAJ98Eq9TZvtxHsU"
 rickroll_link = "https://tinyurl.com/yc665n9y"
 referal_phrase = "potato"
 victims_preview = {
-    "thumbnail": "/home/weirdoo/project/CLI/silent rick /test.jpg",
+    "thumbnail": "/test.jpg",
     "text": "Бесплатная база данных курсов по Python",
     "btn_text": "Канал"
 }
